2c3555a34e5144a7b1abaea505cc2eb7d8eba89e
Everpe <ever.penab@autonoma.edu.co>
2022-09-26 21:46:16 -0500

    fix: borrar codigo innecesario

--- BlazorPeliculas/Client/Repositorios/Repositorio.cs
+++ BlazorPeliculas/Client/Repositorios/Repositorio.cs
@@ -4,14 +4,20 @@ namespace BlazorPeliculas.Client.Repositorios
 {
     public class Repositorio : IRepositorio
     {
+
         public List<Pelicula> ObtenerPeliculas()
         {
             return new List<Pelicula>()
-            {
-                new Pelicula { Titulo = "<b>Mi Pelicula1</b>", Lanzamiento = DateTime.UtcNow },
-                new Pelicula { Titulo = "<i>Mi Pelicula2</i>", Lanzamiento = DateTime.UtcNow },
-                new Pelicula { Titulo = "Mi Pelicula3", Lanzamiento = DateTime.UtcNow }
+{
+                new Pelicula(){Titulo = "Spider-Man: Far From Home",
+                    Lanzamiento  = new DateTime(2019, 7, 2),
+                    Poster = "https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg"},
+                new Pelicula(){Titulo = "Moana", Lanzamiento  = new DateTime(2016, 11, 23),
+                    Poster = "https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_UX182_CR0,0,182,268_AL_.jpg"},
+                new Pelicula(){Titulo = "Inception", Lanzamiento  = new DateTime(2010, 7, 16),
+                    Poster = "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg"}
             };
         }
+
     }
 }
