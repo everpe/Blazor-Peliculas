78bc7902f6ae5460530e19e2b2c814e02df66f66
Everpe <ever.penab@autonoma.edu.co>
2022-09-28 15:29:25 -0500

    add Formularios and components

---
+++ BlazorPeliculas/Shared/Entidades/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Shared.Entidades
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        public string Biografia { get; set; }
+        public string Foto { get; set; }
+        [Required]
+        public DateTime? FechaNacimiento { get; set; }
+
+        //Determina cuando una persona es igual a otra
+        public override bool Equals(object obj)
+        {
+            if (obj is Persona p2)
+            {
+                return Id == p2.Id;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
